67892b781b9def5992d12fd77b3cd2e3ad5e1422
Konstantin <diablopc@ya.ru>
2017-01-18 02:41:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,46 +23,46 @@ fi
 ##FUNC-----------------------------------------------------------------------------------------
 #AUTENTICATION
 AUTH ()
-		{
-		csrf=$(curl http://linuxforum.ru/login.php | grep csrf_token | sed 's/.*value="//' | sed 's/".*//')
-		curl -X POST -c cookie "http://linuxforum.ru/login.php" --data "form_sent=1&csrf_token=$csrf&req_username=$USR&req_password=$PWD&login=Войти"
-		}
+	{
+	csrf=$(curl http://linuxforum.ru/login.php | grep csrf_token | sed 's/.*value="//' | sed 's/".*//')
+	curl -X POST -c cookie "http://linuxforum.ru/login.php" --data "form_sent=1&csrf_token=$csrf&req_username=$USR&req_password=$PWD&login=Войти"
+	}
 
 #GET AND SHOW USER INFO
 SHOWUSER ()
-			{
-			LASTUSERDATA=`curl -b cookie "$LASTULINK" |\
-								grep 'h2 class="hn"' -A35 |\
-								sed 's/.*<div.*//g' |\
-								sed 's/<.*\/div.*>//g' |\
-								sed 's/.*<ul.*//g' |\
-								sed 's/.*<\/ul>.*//g' |\
-								sed 's/<li>//g' |\
-								grep -v "^$" |\
-								sed 's/<h2 class="hn">//' |\
-								sed 's/<\/h2>//'`
-			notify-send -t 0 --icon="$ICON" "$(echo -e "<b>Регистрация нового пользователя</b>")" "$(echo -e "$LASTUSERDATA")"
-			echo "$LASTUNAME" > $WORKDIR/last_user
-			PRELASTUNAME=`echo "$LASTUNAME"`
-			}
+	{
+	LASTUSERDATA=`curl -b cookie "$LASTULINK" |\
+			grep 'h2 class="hn"' -A35 |\
+			sed 's/.*<div.*//g' |\
+			sed 's/<.*\/div.*>//g' |\
+			sed 's/.*<ul.*//g' |\
+			sed 's/.*<\/ul>.*//g' |\
+			sed 's/<li>//g' |\
+			grep -v "^$" |\
+			sed 's/<h2 class="hn">//' |\
+			sed 's/<\/h2>//'`
+	notify-send -t 0 --icon="$ICON" "$(echo -e "<b>Регистрация нового пользователя</b>")" "$(echo -e "$LASTUSERDATA")"
+	echo "$LASTUNAME" > $WORKDIR/last_user
+	PRELASTUNAME=`echo "$LASTUNAME"`
+	}
 
 #CREATE AND SHOW MESSAGE
 SHOWMSG ()
-			{	
-			MESSAGE=`curl -b cookie $ALINK | grep "id=\"post$POST\"" -A1000 | grep 'class="postfoot"' -B1000 `
-			notify-send -t 0 --icon="$ICON" "$(echo -e "<b>@$LFUSER</b> $HEADER <br />$TOPICNAME")" "$(echo -e "$MESSAGE")"
-         echo "$MSGCOUNT" > $WORKDIR/last_topic
-         echo "$TOPICNAME" >> $WORKDIR/last_topic
-         PREMSGCOUNT=`echo "$MSGCOUNT"`
-         PRETOPICNAME=`echo "$TOPICNAME"`
-			}
+	{	
+	MESSAGE=`curl -b cookie $ALINK | grep "id=\"post$POST\"" -A1000 | grep 'class="postfoot"' -B1000 `
+	notify-send -t 0 --icon="$ICON" "$(echo -e "<b>@$LFUSER</b> $HEADER <br />$TOPICNAME")" "$(echo -e "$MESSAGE")"
+	echo "$MSGCOUNT" > $WORKDIR/last_topic
+	echo "$TOPICNAME" >> $WORKDIR/last_topic
+	PREMSGCOUNT=`echo "$MSGCOUNT"`
+	PRETOPICNAME=`echo "$TOPICNAME"`
+	}
 
 while true
 do
 {
 ##CHECK AUTH AND AUTENTICATE
 		CHECKAUTH=`curl -b cookie http://linuxforum.ru/index.php |\
-					grep "Пожалуйста, войдите или зарегистрируйтесь."`
+			grep "Пожалуйста, войдите или зарегистрируйтесь."`
 
 		if [ "$CHECKAUTH" != "" ]; then
 			AUTH
@@ -70,9 +70,9 @@ do
 
 ##GET NEW USERS DATA
 		if [ "$CHECKUSERS" == "1" ]; then
-				LASTUSERREG=`curl -b cookie http://linuxforum.ru/index.php | grep "Последним зарегистрировался"`
-				LASTUNAME=`echo "$LASTUSERREG" | sed 's/.*profile.php?id=.*">//' | sed 's/<\/a>.*//'`
-				LASTULINK=`echo "$LASTUSERREG" | sed 's/.*href="//' | sed 's/">.*//'`
+			LASTUSERREG=`curl -b cookie http://linuxforum.ru/index.php | grep "Последним зарегистрировался"`
+			LASTUNAME=`echo "$LASTUSERREG" | sed 's/.*profile.php?id=.*">//' | sed 's/<\/a>.*//'`
+			LASTULINK=`echo "$LASTUSERREG" | sed 's/.*href="//' | sed 's/">.*//'`
 		fi
 
 ##GET DATA
@@ -80,21 +80,21 @@ do
 
 ##SORT DATA
 		MSGCOUNT=`echo "$GETINDEX" | grep main-first-item -A10 |\
-					grep info-replies |\
-					sed 's/.*<strong>//' |\
-					sed 's/<\/strong.*//'`
+				grep info-replies |\
+				sed 's/.*<strong>//' |\
+				sed 's/<\/strong.*//'`
 		TOPICNAME=`echo "$GETINDEX" | grep main-first-item -A10 |\
-					grep 'class="item-num"' |\
-					sed 's/.*viewtopic.php.*">//' |\
-					sed 's/<.*//'`
+				grep 'class="item-num"' |\
+				sed 's/.*viewtopic.php.*">//' |\
+				sed 's/<.*//'`
 		LFUSER=`echo "$GETINDEX" | grep main-first-item -A10 |\
-					grep info-lastpost |\
-					sed 's/.*<cite>//' |\
-					sed 's/<\/cite.*//'`
+				grep info-lastpost |\
+				sed 's/.*<cite>//' |\
+				sed 's/<\/cite.*//'`
 		TOPICSTARTER=`echo "$GETINDEX" | grep main-first-item -A10 |\
-							grep item-starter |\
-							sed 's/.*<cite>//' |\
-							sed 's/<\/.*//'`
+				grep item-starter |\
+				sed 's/.*<cite>//' |\
+				sed 's/<\/.*//'`
 		ALINK=`echo "$GETINDEX" | grep main-first-item -A10 |\
 				grep info-lastpost |\
 				sed 's/.*<a.href="//' |\
